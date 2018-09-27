2e7f69c551cbb9b767748e1a89170badb9e99c45
Trevor Snyder <trevsny@gmail.com>
2018-09-26 23:14:48 -0500

    Ajax calls for filtering db

--- apps/concertFinder/admin.py
+++ apps/concertFinder/admin.py
@@ -4,6 +4,12 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
-from .models import *
-admin.site.register(ConcertVenue)
-admin.site.register(ConcertInfo)
+from .models import ConcertInfo, ConcertVenue
+# admin.site.register(ConcertVenue)
+# admin.site.register(ConcertInfo)
+@admin.register(ConcertInfo)
+class ConcertAdmin(admin.ModelAdmin):
+    list_display = ['venue','artist','month','day','year']
+@admin.register(ConcertVenue)
+class Venue(admin.ModelAdmin):
+    list_display = ['venue_name']
