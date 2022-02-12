9a2271993a74ae6c55080b354e3611c211e6c4ed
Trevor Snyder <trevsny@gmail.com>
2022-02-11 18:02:37 -0800

    Commented back in the postgresql server

--- utahConcertReview/settings.py
+++ utahConcertReview/settings.py
@@ -77,14 +77,14 @@ WSGI_APPLICATION = 'utahConcertReview.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        # 'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': 'mydatabase',
-        # 'USER': 'mydatabaseuser',
-        # 'PASSWORD': 'mypassword',
-        # 'HOST': '127.0.0.1',
-        # 'PORT': '5432'
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'mydatabaseuser',
+        'PASSWORD': 'mypassword',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
