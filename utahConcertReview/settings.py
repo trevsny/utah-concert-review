9d0760341a2eda51dd840b0036355bc46784f185
Trevor Snyder <trevsny@gmail.com>
2022-02-16 15:54:49 -0800

    Commented out a static files line in settings.py

--- utahConcertReview/settings.py
+++ utahConcertReview/settings.py
@@ -131,5 +131,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-    '/var/www/static',
+    # '/var/www/static',
 )
