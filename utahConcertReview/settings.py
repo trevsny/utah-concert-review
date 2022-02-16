1ae7407b9ff90f893ceb880e7f7e33a8ed9cf019
Trevor Snyder <trevsny@gmail.com>
2022-02-15 21:55:44 -0800

    Migrated new postgresql db, changed a package to a previous version to avoid errors.

--- utahConcertReview/settings.py
+++ utahConcertReview/settings.py
@@ -81,8 +81,8 @@ DATABASES = {
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'mydatabase',
-        'USER': 'mydatabaseuser',
-        'PASSWORD': 'mypassword',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432'
     }
