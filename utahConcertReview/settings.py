2f40b763d73358963cb18ace8ffbd43f42019d72
Trevor Snyder <trevsny@gmail.com>
2022-02-16 21:14:42 -0800

    Finally figured out relative path to static files I need

--- utahConcertReview/settings.py
+++ utahConcertReview/settings.py
@@ -133,7 +133,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'utahConcertReview/static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
