44a22ecd0d62eaaf66939639770fac2174977682
Trevor Snyder <trevsny@gmail.com>
2022-02-16 20:25:18 -0800

    Commented out staticfiles_dirs to see if it affected production

--- utahConcertReview/settings.py
+++ utahConcertReview/settings.py
@@ -129,6 +129,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'staticfiles'),
+# )
